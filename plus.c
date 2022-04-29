8ffb4f644d24bed8df4ec3ea78df11eed57c0d8c
jhy3720 <91109413+jhy3720@users.noreply.github.com>
2022-04-29 12:59:30 +0900

    Create plus.c

---
+++ plus.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Insert Number : ");
+    scanf("%d", &a);
+    printf("Insert Number : ");
+    scanf("%d", &b);
+
+    printf("%d + %d = %d \n", a, b, a + b);
+    return 0;
+}
